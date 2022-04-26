4cb70015d519b9d3b496b8b2d692b3588bbf9329
Harry Ren <91446618+harryren17@users.noreply.github.com>
2022-04-25 21:42:20 -0400

    Added freq values
    check error/word pair from SUBTLEXusfreqencyabove1.xls, append to error/target pair if both >1 (only real words in final data)

--- Python/res1.py
+++ Python/res1.py
@@ -5,109 +5,100 @@ import nltk
 import csv
 import string
 
-#nltk.download('punkt')
-
-#list of entries 
+#global
 items = []
+entryCount = 0
 
 #parse JSONL with indent to dict
-def parse(filename):
+def parse(filename,entryCount):
     #open JSONL
     with open(filename,'r',encoding='utf8', errors='ignore') as f:
         #remove first and last brackets
         data = f.read()[1:-2]
         #extract JSON item
         data = data.split('}\n{')
-
         #extract first 200 entires to items 
-        for i in range(200):
-            item = data[i]
+        totalcount = 0
+        while entryCount<200:
+            item = data[totalcount]
             #parse item to python dict type
             item_dict = json.loads('{'+item+'}')
-            #append parsed item to item list
-            items.append(item_dict)
+
+            pair = getTypoFromSentence(item_dict)
+            getfreqs(pair)
+            if(len(pair)==4 and pair[2]!=0 and pair[3]!=0):
+                #append parsed item to item list
+                items.append(pair)
+                entryCount+=1
+            totalcount+=1
 
 #splits sentence into constituent parts (NLTK)
+#takes sentence, returns array of words
 def tokenizeElem(s):
     sentence = s
     tokens = nltk.word_tokenize(sentence)
-    #print(tokens)
     return tokens
 
 #takes two tokenized sentences, returns typo pair
 def getWrongPair(s1,s2):
-    #print("source:")
-   # print(s1)
-    #print("target")
-    #print(s2)
-    #print("error")
     omit = False
     delete = False
     errorPair = ['','']
-    #if number of tokens do not match, omission
+    #if number of tokens do not match, omission exists
     if len(s1)<len(s2):
         omit = True
     if len(s1)<len(s2):
         delete = True
+    #loop through each word in sentence
     for i in range(min(len(s1),len(s2))):
         if s1[i]!=s2[i] and omit:
+            #one word is split into two
             if s1[i] == s2[i]+s2[i+1]:
                 errorPair[0] = s1[i]
                 errorPair[1] = s2[i+1]
-                #print(errorPair)
                 return errorPair
-                break
+            #one word is omitted
             elif s1[i] == s2[i+1]:
                 errorPair[0] = '[omission]'
                 errorPair[1] = s2[i]
-                #print(errorPair)
                 return errorPair
-                break
-            
-
+        #incorrect spelling
         elif s1[i]!=s2[i]:
             errorPair[0] = s1[i]
             errorPair[1] = s2[i]
            # print(errorPair)
             return errorPair
-            break
-
+    #no spelling error, likely spacing error
     errorPair[0] = ''
     errorPair[1] = ''
     return errorPair
 
-
-
-
-        
-
 #takes dict of typos, returns array of typo pairs
-def getTypoFromSentences(list):
+def getTypoFromSentence(item):
+    #array of pairs of error/repair words
     pairs = []
-    lendict = len(list)
-    for i in range(lendict):
-        numMistakes = len(items[i]['edits'])
-        #print(numMistakes)
-        for j in range(numMistakes):
-            src_sent = items[i]['edits'][j]['src']['text']
-            tgt_sent = items[i]['edits'][j]['tgt']['text']
-            #print(src_sent)
-            #print(tgt_sent)
-            tokenedsrc = tokenizeElem(src_sent)
-            tokenedtgt = tokenizeElem(tgt_sent)
-            #print(tokenedsrc)
-            #print(tokenedtgt)
-            wrongPair = getWrongPair(tokenedsrc,tokenedtgt)
-            pairs.append(wrongPair)
+    numMistakes = len(item['edits'])
+    for j in range(numMistakes):
+        #extract incorrected and fixed sentences from dict
+        src_sent = item['edits'][j]['src']['text']
+        tgt_sent = item['edits'][j]['tgt']['text']
+        #tokenize sentences into array of words
+        tokenedsrc = tokenizeElem(src_sent)
+        tokenedtgt = tokenizeElem(tgt_sent)
+        #isolate the error word and fixed word
+        wrongPair = getWrongPair(tokenedsrc,tokenedtgt)
+        #append to 
+        pairs.append(wrongPair)
     return pairs
 
+#converts list of error/repair arrays into csv file
 def writeTocsv(list):
     header = ['mistake','fix']
     with open('results.csv','w',newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(header)
         for i in range(len(list)):
-            #add get freqs
+            #before writing to csv, append frequencies
             getfreqs(list[i])
             print(list[i])
             try:
@@ -121,7 +112,9 @@ def getfreqs(pair):
     tgt = pair[1].lower()
     wb = xlrd.open_workbook('SUBTLEXusfrequencyabove1.xls')
     sheet = wb.sheet_by_index(0)
-    
+
+    #loop through corpus, check for matches with pair words
+    #append found frequencies to pair array
     for i in range(sheet.nrows):
         if src == sheet.cell_value(i,0):
             if len(pair)==4:
@@ -135,20 +128,14 @@ def getfreqs(pair):
                 pair.append(0)
                 pair.append(sheet.cell_value(i,1))
 
-    
 
 if __name__ == '__main__':
-    parse('github-typo-corpus.v1.0.0.jsonl')
-    #print as json
-    #print(json.dumps(items,indent=2))
-    print('\n\n\n')
-    #print 1 entry
-   # print(items[4]['edits'][0]['src']['text'])
-   # print(items[4]['edits'][0]['tgt']['text'])
-   # sen = items[4]['edits'][0]['tgt']['text']
-    data = getTypoFromSentences(items)
-    print(data)
-    writeTocsv(data)
+    #parse jsonl to dict
+    parse('github-typo-corpus.v1.0.0.jsonl',0)
+    #convert dict to array of typo pairs
+    
+    #write array of typo pairs to csv (also add frequencies)
+    writeTocsv(items)
     print('complete')
-   # tokenizeElem(sen)
+
  
